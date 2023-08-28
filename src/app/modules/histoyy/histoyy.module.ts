c2e87ae3dd320387474658a3475f06d111d3a84f
FernandoAlmaraz <fernandoalmarazdelaquintana@gmail.com>
2023-08-28 19:04:16 -0400

    adding modules and scaffolding

---
+++ src/app/modules/histoyy/histoyy.module.ts
@@ -0,0 +1,14 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { HistoyyRoutingModule } from './histoyy-routing.module';
+
+
+@NgModule({
+  declarations: [],
+  imports: [
+    CommonModule,
+    HistoyyRoutingModule
+  ]
+})
+export class HistoyyModule { }
