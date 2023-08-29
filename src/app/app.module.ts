7fbcd80302672d092f2c2e09944e0676d33a5c50
FernandoAlmaraz <fernandoalmarazdelaquintana@gmail.com>
2023-08-28 21:43:22 -0400

    creating components in shared module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,9 +3,8 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-
 @NgModule({
-  declarations: [
+  declarations: [// si es modulo declaras si no es lo dejas, pero puedes exportar
     AppComponent
   ],
   imports: [
